978d6eadfb48edfc92a917102749145e381d92c3
BRIAN CAMILO PÉREZ ROMERO <BPEREZ@XM.COM.CO>
2020-08-05 05:49:17 -0500

    Added cooments on code

--- IPOEMP.py
+++ IPOEMP.py
@@ -7,7 +7,6 @@ from win32com.client import Dispatch
 #==============================================================================
 #Functions.
 #==============================================================================
-
 #-----------------------------------------
 # Get month to study from user.
 def askForMonth():
@@ -24,16 +23,17 @@ def askForMonth():
 #Create Restrictions table from IPOEMP table
 def createRestrictionsTable(month, year, trimester):
     wbIPOEMP = xl.Workbooks.Open(Filename = thisFolderPath + "\\"+IPOEMPfilename)   #Load IPOEMP excel file
-    wbRestricciones = xl.Workbooks.Add()    #Create new excel file
-    wsIPOEMP = wbIPOEMP.Worksheets('Restricciones')
+    wsIPOEMP = wbIPOEMP.Worksheets('Restricciones')                                 #Load 'Restricciones' sheet
+    wbRestricciones = xl.Workbooks.Add()                                            #Create new excel file
+    
+    lastRow = wsIPOEMP.UsedRange.Rows.Count                                                                         #Get last row value    
+    lastCol = [i for i in range(1,wsIPOEMP.UsedRange.Columns.Count) if wsIPOEMP.Cells(1,i).Value=="Corte\n[MW]"][0] #Get Col index for "Corte [MW]"
 
-    lastRow = wsIPOEMP.UsedRange.Rows.Count     #Get last row value    
-    #*This could be improved by removing the 7*
-    lastCol = 7
-    wsIPOEMP.Range("A1", wsIPOEMP.Cells(lastRow, lastCol)).Copy()     #Copy the needed range
+    wsIPOEMP.Range("A1", wsIPOEMP.Cells(lastRow, lastCol)).Copy()               #Copy the needed range
     wbRestricciones.ActiveSheet.Paste(wbRestricciones.ActiveSheet.Cells(1,1))   #Paste the needed range
-    wbRestricciones.ActiveSheet.Cells(1, lastCol+1).Value = "Corte Python"
-    wbRestricciones.ActiveSheet.Cells(1, lastCol+2).Value = "¿Superó corte?"
+    wbRestricciones.ActiveSheet.Cells(1, lastCol+1).Value = "Corte Python"      #Create new column
+    wbRestricciones.ActiveSheet.Cells(1, lastCol+2).Value = "¿Superó corte?"    #Create new column
+    wsIPOEMP.Cells(1, 1).Copy()     #Copy a cell to clear de clipboard (to avoid Excel message)
     wbIPOEMP.Close(False)           #Close IPOEMP file without saving changes
     wbRestricciones.Close(True, thisFolderPath + "\\"+year+"-"+ month+"_Restricciones_"+year+"_T"+str(trimester)+".xlsx") #Close new file saving changes
 
@@ -42,10 +42,9 @@ def createRestrictionsTable(month, year, trimester):
 #Main.
 #==============================================================================
 def main():
-    month, year=askForMonth()
-    trimester = (int(month)-1)//3 +1
-    createRestrictionsTable(month, year, trimester)
-
+    month, year=askForMonth()                       #Get month and year form user
+    trimester = (int(month)-1)//3 +1                #Calculate trimester
+    createRestrictionsTable(month, year, trimester) #Create Restrictions table from IPOEMP table
 
 
 #==============================================================================
